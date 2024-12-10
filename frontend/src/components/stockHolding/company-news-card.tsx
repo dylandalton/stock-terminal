4cf0a3c922f498dba93ac498852cb9e32b5838ae
DESKTOP-79H2PGD\dylan <dylandalton.za@gmail.com>
2024-12-10 16:55:01 +0000

    StockHolding: Updated company news styles

--- frontend/src/components/stockHolding/company-news-card.tsx
+++ frontend/src/components/stockHolding/company-news-card.tsx
@@ -2,13 +2,17 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import NewsArticleCard from "./news-article-card"
 import { useAppSelector } from "@/lib/hooks/typedHooks"
+import { Newspaper } from 'lucide-react';
 
 export default function CompanyNewsCard() {
   const scrapedArticles = useAppSelector((state) => state.scrape.scrapedArticles);
   return (
-    <Card className="w-full max-w-4xl mx-auto">
-      <CardHeader>
-        <CardTitle className="text-2xl font-bold">Latest Company News</CardTitle>
+    <Card className="w-full max-w-4xl my-5">
+      <CardHeader className="pb-2">
+        <div className="flex items-center">
+          <CardTitle className="text-2xl font-bold mr-2">Latest Company News</CardTitle>
+          <Newspaper className="h-6 w-6" />
+        </div>
       </CardHeader>
       <CardContent>
         <ScrollArea className="h-[400px] pr-4">
